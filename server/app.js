84d4d77e200a1d29c0e7c7fda2a477e6172fb52a
Unnati Wankar <unnatiwankar63@gmail.com>
2026-09-03 17:25:19 +0530

    Fix CORS for production frontend

--- server/app.js
+++ server/app.js
@@ -27,14 +27,18 @@ app.use(helmet());
 
 // CORS
 
+const allowedOrigins = [
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
+    "https://professional-portfolio-1-k7j8.onrender.com"
+];
+
 app.use(
-  cors({
-    origin: ["http://localhost:5500", "http://127.0.0.1:5500"],
-
-    methods: ["GET", "POST", "PUT", "DELETE"],
-
-    allowedHeaders: ["Content-Type"],
-  }),
+    cors({
+        origin: allowedOrigins,
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type"]
+    })
 );
 
 // BODY PARSER
